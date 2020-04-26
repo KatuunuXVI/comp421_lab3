e7147d65adc61b8b169941f2b1ad18b8a2582dac
Jungwoo Lee <jeong1135@gmail.com>
2020-04-26 12:14:17 -0500

    Pseudo code for overall structure

--- iolib.c
+++ iolib.c
@@ -1,8 +1,80 @@
 #include <stdio.h>
 #include <comp421/iolib.h>
 
+/*
+ * Given pathname, create a linked list of parsed components
+ */
+// PathIterator *CreatePathIterator(char *pathname);
+
+/*
+ * Free Path Iterator.
+ * Should be able to delete all iterators even if
+ * any step of iterator is provided.
+ */
+// int DeletePathIterator(PathIterator *it);
+
+/*
+ * Prepare new file descriptor using lowest available fd
+ * using file data. Return -1 if open file table is all filled up.
+ */
+// int CreateFileDescriptor();
+
+/*
+ * Close file descriptor. Return -1 if fd is invalid.
+ */
+// int CloseFileDescriptor(int fd);
+
+/*
+ * Get file descriptor. Return NULL if fd is invalid.
+ */
+// FileDescriptor GetFileDescriptor(int fd);
+
+/**
+ * Creates and opens new file named pathname.
+ */
 int Create(char *pathname) {
-    printf("Pathname: %s\n",pathname);
+    TracePrintf(10, "\t┌─ [Create] path: %s\n", pathname);
+    /*
+     * TODO:
+     * - Parse pathname and ensure all non-last components exist
+     * - If named file already exists, set its size as 0 and prepare to override
+     * - Error if creating a file with the same name as existing directory
+     * - Return lowest available file descriptor
+     */
+
+    /*
+    // Verify against pathname max length
+
+    // Create an iterator through tokenized pathname
+    int target_inum;
+    PathIterator *it = CreatePathIterator(pathname);
+    while (it->data != NULL) {
+        Send the following data to file server
+        - Type: GetFile
+        - Arg1: current directory inode
+        - Arg2: next component (it->data)
+        and expect the following data back
+        - Type: GetFile
+        - Data1: target_inum = inode if requested component exists, 0 if not
+        - Break if not found
+
+        it = it->next;
+    }
+
+    DeletePathIterator(it);
+
+    // If target_inum is found
+    // Return ERROR if existing file is directory
+    // Send TruncateFile message to file server
+    // return with file descriptor
+
+    // If target_inum is not found
+    // Return ERROR if not found target_inum is not last component of path
+    // Create New File
+    // return with file descriptor
+    */
+
+    TracePrintf(10, "\t└─ [Create]\n\n");
     return 0;
 }
 
@@ -10,7 +82,33 @@ int Create(char *pathname) {
  * Opens file 'pathname'
  */
 int Open(char *pathname) {
-    printf("Pathname: %s\n",pathname);
+    TracePrintf(10, "\t┌─ [Open] path: %s\n", pathname);
+
+    /*
+    // Verify against pathname max length
+
+    // Create an iterator through tokenized pathname
+    int target_inum;
+    PathIterator *it = CreatePathIterator(pathname);
+    while (it->data != NULL) {
+        Send the following data to file server
+        - Type: GetFile
+        - Arg1: current directory inode
+        - Arg2: next component (it->data)
+        and expect the following data back
+        - Type: GetFile
+        - Data1: target_inum = inode if requested component exists, 0 if not
+
+        it = it->next;
+    }
+
+    DeletePathIterator(it);
+
+    // If target_inum is not found, return ERROR
+
+    // If target_inum is found, return file descriptor
+    */
+    TracePrintf(10, "\t└─ [Open]\n\n");
     return 0;
 }
 
@@ -18,19 +116,38 @@ int Open(char *pathname) {
  * Closes file specified by fd
  */
 int Close(int fd) {
-    printf("File Descriptor: %d\n",fd);
+    TracePrintf(10, "\t┌─ [Close] fd: %d\n", fd);
+    // Throw error if fd is invalid number
+    // Throw error if fd is not currently opened
+    /*
+    if (CloseFileDescriptor(fd) < 0) return ERROR;
+    */
+    TracePrintf(10, "\t└─ [Close]\n\n");
     return 0;
 }
-
-
 
 /**
  * Copies 'size' bytes from file 'fd' to '*buf'.
  */
 int Read(int fd, void *buf, int size) {
-    printf("File Descriptor: %d\n",fd);
+    TracePrintf(10, "\t┌─ [Read] fd: %d\n", fd);
     printf("Size: %d\n",size);
     printf("Address: %p\n",buf);
+    /*
+    // Throw error if fd is invalid number
+    // Throw error if fd is not opened
+
+    Send the following data to file server
+    - Type: ReadFile
+    - arg1: inode (fd's inode)
+    - arg2: offset (fd's current position)
+    - arg3: size
+    - arg4: buf pointer
+    - Expect file server to call CopyTo
+    Expect the following result
+    - data1: read bytes, -1 if error
+    */
+    TracePrintf(10, "\t└─ [Read]\n\n");
     return 0;
 }
 
@@ -38,9 +155,25 @@ int Read(int fd, void *buf, int size) {
  * Copies 'size' bytes from '*buf' to file 'fd'.
  */
 int Write(int fd, void *buf, int size) {
-    printf("File Descriptor: %d\n",fd);
-    printf("Size: %d\n",size);
-    printf("Address: %p\n",buf);
+    TracePrintf(10, "\t┌─ [Write] fd: %d\n", fd);
+    printf("Size: %d\n", size);
+    printf("Address: %p\n", buf);
+    /*
+    // Throw error if fd is invalid number
+    // Throw error if fd is not opened
+
+    Send the following data to file server
+    - Type: WriteFile
+    - arg1: inode (fd's inode)
+    - arg2: offset (fd's current position)
+    - arg3: size
+    - arg4: buf pointer
+    - Expect file server to call CopyFrom
+    - Seek can travel beyond end of file to create hole.
+    Expect the following result
+    - data1: written bytes, -1 if error
+    */
+    TracePrintf(10, "\t└─ [Write]\n\n");
     return 0;
 }
 
@@ -48,9 +181,16 @@ int Write(int fd, void *buf, int size) {
  * Changes position of the open file
  */
 int Seek(int fd, int offset, int whence) {
-    printf("File Descriptor: %d\n",fd);
-    printf("Offset: %d\n",offset);
-    printf("Whence: %d\n",whence);
+    TracePrintf(10, "\t┌─ [Seek] fd: %d\n", fd);
+    printf("Offset: %d\n", offset);
+    printf("Whence: %d\n", whence);
+    /*
+    // Throw error if fd is invalid number
+    // Throw error if fd is not opened
+    // This call may not required file server
+    // if local fd remembers all file metadata
+    */
+    TracePrintf(10, "\t└─ [Seek]\n\n");
     return 0;
 }
 
--- yfs.c
+++ yfs.c
@@ -31,15 +31,15 @@ void *GetBlock(int block_num) {
     /** First Check Block Cache*/
     int found = 0;
     struct block_cache_entry *current = block_stack->top;
-    while(current != NULL && !found) {
+    while (current != NULL && !found) {
         found = (current->block_number == block_num);
         current = current->next;
     }
-    if(found) return current;
+    if (found) return current;
 
     /** If not found in cache, read directly from disk */
     void *block_buffer = malloc(SECTORSIZE);
-    ReadSector(block_num,block_buffer);
+    ReadSector(block_num, block_buffer);
     return block_buffer;
 }
 
@@ -50,19 +50,18 @@ struct inode* GetInode(int inode_num) {
     /** First Check the Inode Cache */
     struct inode_cache_entry *current = inode_stack->top;
     int found = 0;
-    while(current != NULL && !found) {//Searches until it runs to the bottom of the stack
+    while (current != NULL && !found) {//Searches until it runs to the bottom of the stack
         found = (current->inode_number == inode_num);
         current = current->next;
     }
-    if(found) {
+    if (found) {
         RaiseInodeCachePosition(inode_stack,current);
         return current->in;
     }
     /**If it's not in the Inode Cache, check the Block */
-    void* inode_block = GetBlock((inode_num / 8) + 1);
+    void *inode_block = GetBlock((inode_num / 8) + 1);
     AddToInodeCache(inode_stack,(struct inode *)inode_block + (inode_num % 8),inode_num); /**Add inode to cache, when accessed*/
     return (struct inode *)inode_block + (inode_num % 8);
-
 }
 
 /**
@@ -71,9 +70,9 @@ struct inode* GetInode(int inode_num) {
 void GetFreeInodeList() {
     free_inode_list = getBuffer(header->num_inodes);
     int i;
-    for(i = 1; i <= header->num_inodes; i ++) {
+    for (i = 1; i <= header->num_inodes; i ++) {
         struct inode *next = GetInode(i);
-        if(next->type == INODE_FREE) pushToBuffer(free_inode_list,i);
+        if (next->type == INODE_FREE) pushToBuffer(free_inode_list,i);
     }
 }
 
@@ -81,24 +80,50 @@ void GetFreeInodeList() {
 
 int main(int argc, char **argv) {
     Register(FILE_SERVER);
-    /** Obtain File System Header */
+
+    /* Obtain File System Header */
     void *sector_one = malloc(SECTORSIZE);
-    if(ReadSector(1,sector_one) == 0) {
+    if (ReadSector(1, sector_one) == 0) {
         header = (struct fs_header *)sector_one;
     } else {
         printf("Error\n");
     }
-    /**Obtain Root Directory Inode*/
+
+    /* Obtain Root Directory Inode*/
     root_inode = (struct inode *)header + 1;
+
     inode_stack = CreateInodeCache();
     block_stack = CreateBlockCache();
     GetFreeInodeList();
-    printBuffer(free_inode_list);
-    PrintInodeCache(inode_stack);
+    // printBuffer(free_inode_list);
+    // PrintInodeCache(inode_stack);
+
+    int i;
+    struct dir_entry *block7 = GetBlock(7);
+    printf("block7->inum: %d\n", block7->inum);
+    printf("block7->name: %s\n", block7->name);
+
+    int pid;
+  	if ((pid = Fork()) < 0) {
+  	    TracePrintf(0, "Cannot Fork.\n");
+  	    return -1;
+  	}
+
+    /* Child process exec program */
+    if (pid == 0) {
+        GetBlock(7);
+        Exec(argv[1], argv + 1);
+        TracePrintf(0, "Cannot Exec.\n");
+        return -1;
+    }
+
+    void *buffer = malloc(32);
+    while (1) {
+        if ((pid = Receive(buffer)) < 0) {
+            TracePrintf(0, "Receive Error.\n");
+            return -1;
+        }
+    }
+
     return 0;
 };
-
-
-
-
-
